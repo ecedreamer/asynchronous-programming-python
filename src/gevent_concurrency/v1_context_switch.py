1d02d2db08b669ae2c1946d699ab5b74f85eefd1
ecedreamer <sangit.niroula@gmail.com>
2022-08-12 22:59:45 +0545

    gevent

--- src/gevent_concurrency/v1_context_switch.py
+++ src/gevent_concurrency/v1_context_switch.py
@@ -1,15 +1,23 @@
 import gevent
+from gevent import monkey
+monkey.patch_all()  # To see gevent context switch, comment/uncomment this line and run the code. 
+import requests
 
 
 def service1():
-    print('Running service1')
+    print('First line of service1')
+    resp = requests.get("https://google.com")
+    print(resp.status_code, "------------")
+    print('Second line of service1')
     gevent.sleep(2)
-    print('Remaining task of service1')
+    print('Third line of service1')
 
 def service2():
-    print('Running Service2')
-    gevent.sleep(4)
-    print('Remaining task of service2')
+    print('First line of service2')
+    print('Second line of service2')
+    gevent.sleep(3)
+    print('Third line of service2')
+
 
 
 def main():
